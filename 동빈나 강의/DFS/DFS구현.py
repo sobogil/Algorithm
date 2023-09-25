0dc947634c0c217a663d0ad1d08738d96218f331
bogil SO <sotomas232@gmail.com>
2023-09-25 23:31:02 +0900

    new commit

---
+++ 동빈나 강의/DFS/DFS구현.py
@@ -0,0 +1,10 @@
+def dfs (graph, v, visited):
+
+    visited[v] = True
+    print(v,end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+graph = []
+visited = [False]*9
