954e9a6935635e4ef3b03503fa0e6120fa509079
bogil SO <sotomas232@gmail.com>
2024-01-07 23:23:43 +0900

    1/7

--- 백준/dp/17626제곱.py
+++ 백준/dp/17626제곱.py
@@ -2,9 +2,9 @@ n=int(input())
 
 dp=[0,1]
 for i in range(2,n+1):
-    min_L=1e9
+    min_L=4
     j=1
-    while (j**2)<=1:
+    while (j**2)<=i:
         min_L=min(min_L,dp[i-j**2])
         j+=1
     dp.append(min_L+1)
