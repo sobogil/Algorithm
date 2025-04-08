13be7bf4dd8a65de0c48be9b84c712a12e9fcacd
Bogil So <sotomas232@gmail.com>
2025-04-08 20:26:42 +0900

    [level 0] Title: 더 크게 합치기, Time: 10.65 ms, Memory: 95.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int ab = Integer.parseInt(""+a+b);
+        int ba = Integer.parseInt(""+b+a);
+        
+        return Math.max(ab,ba);
+    }
+}
