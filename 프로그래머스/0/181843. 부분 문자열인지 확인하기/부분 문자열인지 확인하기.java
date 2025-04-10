0ca7e2028dce6667a514eed7b20c9b5f2a803db0
Bogil So <sotomas232@gmail.com>
2025-04-10 23:29:15 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        return my_string.contains(target)? 1:0;
+
+    }
+}
