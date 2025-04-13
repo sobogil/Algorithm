096eb7d8d3463a064e4401ec667ce92066f1a552
Bogil So <sotomas232@gmail.com>
2025-04-13 13:41:55 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,li[100004],a[100004],len;
+
+int main(){
+    cin >>n;
+    for(int i=0;i<n;i++)cin >>a[i];
+
+    for(int i=0;i<n;i++){
+        auto  pos = lower_bound(li,li+len,a[i]);
+        if(*pos==0)len++;
+        *pos=a[i];
+    }
+    cout << n-len;
+}
