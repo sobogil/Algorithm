d7a601911cbf2adbff7161f345192f72195f0e89
Bogil So <sotomas232@gmail.com>
2025-04-18 17:43:15 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,24 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n,a[1004],dp[1004][1004];
 
-int go(int k,int now){
-    if(k>n){
-        return 0;
-    }
-    int &ret = dp[k][now];
-    if(ret) return ret;
-    if(a[k] > now)
-    ret = max(go(k+1,now), go(k+1,a[k])+ a[k]);
-    else ret = go(k+1,now);
-    return ret;
-}
-int main(){
-    cin >>n;
+int n;
+int a[1001];
+int dp[1001];
 
-    for(int i=1;i<=n;i++){
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
+        dp[i] = a[i]; // 초기값: 자기 자신만 사용하는 경우
     }
 
-    cout << go(0,0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i]) {
+                dp[i] = max(dp[i], dp[j] + a[i]);
+            }
+        }
+    }
+
+    cout << *max_element(dp, dp + n);
 }
