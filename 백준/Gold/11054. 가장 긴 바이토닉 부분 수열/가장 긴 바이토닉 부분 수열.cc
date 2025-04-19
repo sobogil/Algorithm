fce119ba4c0174db8fddb77258a5d7ca0e891fb3
Bogil So <sotomas232@gmail.com>
2025-04-19 20:53:52 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -1,32 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n,a[1004],dp[1004][1004][2],flag;
+int n, a[1004];
+int dp[1004][2]; // dp[i][0]: 증가 상태, dp[i][1]: 감소 상태
 
-int go(int k,int prev,int flag){
-    if(k>=n){
-        return 0;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = dp[i][1] = 1; // 자기 자신만 포함
+
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i]) {
+                // 증가 상태일 때만 가능
+                dp[i][0] = max(dp[i][0], dp[j][0] + 1);
+            }
+            if (a[j] > a[i]) {
+                // 감소 상태는 증가 or 감소 둘 다에서 올 수 있음
+                dp[i][1] = max(dp[i][1], max(dp[j][0], dp[j][1]) + 1);
+            }
+        }
+
+        answer = max({answer, dp[i][0], dp[i][1]});
     }
 
-    int &ret = dp[k][prev][flag];
-    if(ret) return ret;
-
-    if(a[k] > prev && !flag)
-    ret = max(ret, go(k+1,a[k],0)+1);
-    if(a[k] < prev){
-        ret = max(ret, go(k+1,a[k],1)+1);
-    }
-    ret = max(ret, go(k+1,prev,flag));
-
-    return ret;
-}
-
-int main(){
-    cin >>n;
-
-    for(int i=0;i<n;i++){
-        cin >> a[i];
-    }
-
-    cout << go(0,0,0);
+    cout << answer << '\n';
 }
