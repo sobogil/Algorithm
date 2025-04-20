f1701b22f6a7552b45c06215f431e888e2bb83f4
Bogil So <sotomas232@gmail.com>
2025-04-20 13:54:48 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -1,24 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n, a[1004],dp[1004][1004];
+int n, a[1004],dp[1004];
 
 
-int go(int k, int prev){
-    if(k>=n)return 0;
-    int &ret = dp[k][prev];
-    if(ret) return ret;
-
-    if(prev > a[k])
-    ret = max(ret, go(k+1,a[k])+ 1);
-    ret = max(ret, go(k+1,prev));
-    return ret;
-}
 
 int main(){
     cin >> n;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         cin >> a[i];
+        dp[i] = 1;
     }
 
-    cout << go(0,*max_element(a,a+n)+1);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(a[j] > a[i])
+            dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    cout << *max_element(dp,dp+n+1);
 }
