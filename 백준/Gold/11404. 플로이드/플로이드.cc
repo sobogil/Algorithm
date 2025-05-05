bcedb0482b009526c3c7b086b2023bc516a1d761
Bogil So <sotomas232@gmail.com>
2025-05-06 00:33:01 +0900

    [Gold IV] Title: 플로이드, Time: 80 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int n,m,dist[101][101];
+int INF = 987654321;
+
+int main(){
+    cin >>n>>m;
+    for(int i=0;i<n;i++)
+        fill(dist[i],dist[i]+n,INF);
+
+    for(int i=0;i<n;i++){
+        dist[i][i] = 0;
+    }
+
+    for(int i=0;i<m;i++){
+        int a,b,c;
+        cin >>a>>b>>c;
+        a--;b--;
+        dist[a][b] = min(dist[a][b],c);
+    }
+
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dist[i][j] = min(dist[i][k] + dist[k][j],dist[i][j]);
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(dist[i][j]==INF)cout << "0 ";
+            else cout << dist[i][j]<<' ';
+        }
+        cout <<'\n';
+    }
+}
