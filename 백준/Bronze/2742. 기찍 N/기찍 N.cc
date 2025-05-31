f8684638f83e04745c41eb33be0ce1dff5ed141f
Bogil So <sotomas232@gmail.com>
2025-05-31 20:44:35 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+    ll n,m;
+    cin >>n;
+    while(1){
+        cout <<n<<'\n';
+        n--;
+        if(n==0)break;
+    }
+}
